afb7762a9fe540823e7b8907e16178e90fded761
jason <jasonyang.final@gmail.com>
2020-09-15 12:47:25 +0900

    update

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 currdir=`pwd`
 
 cd VagrantFolder/ubuntu1804
-sudo vagrant up
-sudo vagrant ssh "ls -al"
+sudo vagrant up --provider libvirt
+sudo vagrant ssh -c "cd /vagrant && ./init.sh && ./compile.sh"
 
