cc4587f285af393c161f9b85723ccb76e3dc5810
jason <jasonyang.final@gmail.com>
2020-09-18 12:32:40 +0900

    update

--- run.sh
+++ run.sh
@@ -4,9 +4,5 @@ currdir=`pwd`
 
 cd VagrantFolder/ubuntu1804
 sudo vagrant up --provider libvirt
-if [ ! -d "share" ];then
-    mkdir share
-    chmod -R 777 share
-fi
 sudo vagrant ssh -c "cd /vagrant && ./init.sh && ./compile.sh && cp ~/compile/dependency.tar.gz /vagrant"
 
